e49116ddcbdfa306f5a0ad1750e86261b21efcd9
jgoping <jgoping@gmail.com>
2021-05-04 11:14:59 -0400

    feat(client): Add create-react-app src files

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import socketIOClient from "socket.io-client";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Cooldown Chess
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
