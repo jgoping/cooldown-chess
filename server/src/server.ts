a4c43ff0ad4f52b7a8ddf9eef1f6e76a281023bb
jgoping <jgoping@gmail.com>
2021-05-07 21:28:14 -0400

    feat(server): implement basic chess move logic

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,4 @@
+import { Chess } from 'chess.js';
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
@@ -12,11 +13,21 @@ app.get('/', (_req, res) => {
   res.send('Testing');
 });
 
+const chess = new Chess()
 io.on('connection', socket => {
   console.log('Connected');
+    socket.emit('Player', chess.fen());
 
   socket.on('disconnect', () => {
     console.log('Disconnected');
+    });
+  // Listen for moves
+  socket.on('move', (data) => {
+    chess.move({ from: data.sourceSquare, to: data.targetSquare });
+    io.emit('move', chess.fen());
+    if (chess.game_over()) {
+      io.emit('gameOver');
+    }
   });
 });
 
