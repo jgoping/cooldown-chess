e896280cefb9ced8b02daf1243bfeb098d1588cb
jgoping <jgoping@gmail.com>
2021-05-08 14:43:29 -0400

    feat(server): create Room class

---
+++ server/src/room.ts
@@ -0,0 +1,35 @@
+import { Chess } from "chess.js";
+import { Server, Socket } from "socket.io";
+
+class Room {
+  chess = new Chess();
+  io: Server;
+  
+  playerCount = 0;
+  roomId: string;
+
+  constructor(io: Server, roomId: string) {
+    this.io = io;
+    this.roomId = roomId;
+  }
+
+  addPlayer(socket: Socket) {
+    if (this.playerCount < 2) {
+      socket.join(this.roomId);
+
+      ++this.playerCount;
+      socket.emit('Player', this.playerCount === 1 ? 'w' : 'b');
+      socket.emit('Board', this.chess.fen());
+
+      socket.on('Move', (data) => {
+        this.chess.move({ from: data.sourceSquare, to: data.targetSquare });
+        this.io.emit('Board', this.chess.fen());
+        if (this.chess.game_over()) {
+          this.io.emit('GameOver');
+        }
+      });
+    }
+  }
+}
+
+export default Room;
