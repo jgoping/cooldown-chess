52a698c9e5c59855426bda8761f21c66842789eb
jgoping <jgoping@gmail.com>
2021-05-08 14:45:51 -0400

    feat(server): allow users to connect to rooms

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,8 @@
-import { Chess } from 'chess.js';
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
+
+import Room from './room';
 
 const app = express();
 const server = http.createServer(app);
@@ -9,33 +10,17 @@ const io = new Server(server);
 
 const PORT = 8080;
 
-app.get('/', (_req, res) => {
-  res.send('Testing');
-});
 
-const chess = new Chess()
-let playerCount = 0;
+const roomMap = new Map();
 
 io.on('connection', socket => {
-  console.log('Connected');
-  if (playerCount < 2) {
-    ++playerCount;
-    socket.emit('Player', playerCount === 1 ? 'w' : 'b');
-    socket.emit('Board', chess.fen());
-
-  socket.on('disconnect', () => {
-    console.log('Disconnected');
-      --playerCount;
-    });
-  }
-  
-  // Listen for moves
-  socket.on('move', (data) => {
-    chess.move({ from: data.sourceSquare, to: data.targetSquare });
-    io.emit('Board', chess.fen());
-    if (chess.game_over()) {
-      io.emit('gameOver');
+  socket.on('Room', (roomId) => {
+    if (!roomMap.has(roomId)) {
+      roomMap.set(roomId, new Room(io, roomId));
     }
+
+    const room = roomMap.get(roomId);
+    room.addPlayer(socket);
   });
 });
 
