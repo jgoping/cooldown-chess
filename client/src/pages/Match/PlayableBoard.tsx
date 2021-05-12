1e060b6b3af6f26dcf01ddbd454055e01dc63861
jgoping <jgoping@gmail.com>
2021-05-12 10:32:09 -0400

    refactor(client): move socket, state, and timer logic from PlayableBoard to Match

--- client/src/pages/Match/PlayableBoard.tsx
+++ client/src/pages/Match/PlayableBoard.tsx
@@ -1,53 +1,14 @@
 import Chessboard, { Piece } from 'chessboardjsx';
 import React from 'react';
-import socketIOClient, { Socket } from "socket.io-client";
-
-const ENDPOINT = 'http://127.0.0.1:8080/';
+import { Socket } from "socket.io-client";
 
 interface PlayableBoardProps {
-  roomId: string;
+  socket?: Socket;
+  player: string;
+  position: string;
 }
 
-const PlayableBoard: React.FC<PlayableBoardProps> = ({ roomId }) => {
-  const [player, setPlayer] = React.useState('');
-  const [position, setPosition] = React.useState('');
-  const [socket, setSocket] = React.useState<Socket | undefined>();
-  const [playerTimer, setPlayerTimer] = React.useState(0);
-  const [opponentTimer, setOpponentTimer] = React.useState(0);
-
-  React.useEffect(() => {
-    const socket = socketIOClient(ENDPOINT, {transports: ['websocket']});
-    setSocket(socket);
-
-    socket.emit('Room', roomId);
-
-    socket.on('Player', data => {
-      setPlayer(data);
-    });
-
-    socket.on('Board', data => {
-      setPosition(data);
-    });
-
-    socket.on('GameOver', () => {
-      console.log('game is over');
-    });
-
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
-
-  React.useEffect(() => {
-    socket?.on('Time', data => {
-      updateTimer(data);
-    });
-  }, [player])
-
-  const updateTimer = ({ colour, time }: { colour: string, time: number}) => {
-    player === colour ? setPlayerTimer(time) : setOpponentTimer(time);
-  };
-
+const PlayableBoard: React.FC<PlayableBoardProps> = ({ socket, player, position }) => {
   const allowDrag = ({ piece }: { piece: Piece }) => {
     const pieceColour = piece[0];
     return player === pieceColour;
@@ -69,8 +30,6 @@ const PlayableBoard: React.FC<PlayableBoardProps> = ({ roomId }) => {
       onDrop={onDrop}
       orientation={player === 'w' ? 'white' : 'black'}
     />
-    <div>{opponentTimer}</div>
-    <div>{playerTimer}</div>
   </>
   );
 };
--- client/src/pages/Match/index.tsx
+++ client/src/pages/Match/index.tsx
@@ -1,20 +1,71 @@
+import socketIOClient, { Socket } from "socket.io-client";
+import React from 'react';
 import { useParams } from 'react-router-dom';
 
 import PlayableBoard from './PlayableBoard';
+
+const ENDPOINT = 'http://127.0.0.1:8080/';
 
 interface ParamTypes {
   roomId: string;
 }
 
-export const Match = () => {
+const Match = () => {
+  const [socket, setSocket] = React.useState<Socket | undefined>();
+  const [player, setPlayer] = React.useState('');
+  const [winner, setWinner] = React.useState('');
+  const [position, setPosition] = React.useState('');
   const { roomId } = useParams<ParamTypes>();
+  const [playerTimer, setPlayerTimer] = React.useState(0);
+  const [opponentTimer, setOpponentTimer] = React.useState(0);
+
+  React.useEffect(() => {
+    const socket = socketIOClient(ENDPOINT, {transports: ['websocket']});
+    setSocket(socket);
+
+    socket.emit('Room', roomId);
+
+    socket.on('Player', data => {
+      setPlayer(data);
+    });
+
+    socket.on('Begin', () => {
+      setWaitingModalOpen(false);
+    });
+
+    socket.on('Board', data => {
+      setPosition(data);
+    });
+
+    socket.on('GameOver', data => {
+      setGameOverModalOpen(true);
+      setWinner(data);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  React.useEffect(() => {
+    socket?.on('Time', data => {
+      updateTimer(data);
+    });
+  }, [player])
+
+  
+  const updateTimer = ({ colour, time }: { colour: string, time: number}) => {
+    player === colour ? setPlayerTimer(time) : setOpponentTimer(time);
+  };
   
   return (
     <>
       <p>
         Cooldown Chess
       </p>
-      <PlayableBoard roomId={roomId} />
+      <PlayableBoard socket={socket} player={player} position={position} />
+      <div>{opponentTimer}</div>
+      <div>{playerTimer}</div>
     </>
   );
 };
