c69e1bff10259f7c6cbefb3557df9cb13ca8743d
jgoping <jgoping@gmail.com>
2021-05-14 21:44:06 -0400

    style(client): create modal footer styled component

---
+++ client/src/pages/Match/ModalFooter.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const ModalFooter = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  margin-top: 20px;
+`;
+
+export default ModalFooter;
